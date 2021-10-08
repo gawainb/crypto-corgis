8cee13f8a31eb580d825e2dbb48f35d838a1fcb6
G2 <bracy.gawain2014@gmail.com>
2021-10-08 06:37:33 -0400

    Updated hardhat.config
    Added Mumbai and Polygon to the list of networks.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -22,7 +22,11 @@ const config = {
 
 const {
   ETH_RPC_URL_RINKEBY,
+  POLYGON_RPC_URL_MUMBAI,
+  POLYGON_RPC_URL_MATIC,
   PRIVATE_KEY_RINKEBY,
+  PRIVATE_KEY_MUMBAI,
+  PRIVATE_KEY_MATIC,
   ETHERSCAN_API_KEY,
   PRIVATE_KEY_MAINNET,
   ETH_RPC_URL_MAINNET,
@@ -43,6 +47,20 @@ if (ETH_RPC_URL_MAINNET && PRIVATE_KEY_MAINNET) {
   };
 }
 
+if (POLYGON_RPC_URL_MUMBAI && PRIVATE_KEY_MUMBAI) {
+  (config.networks as any).mumbai = {
+    url: POLYGON_RPC_URL_MUMBAI,
+    accounts: [PRIVATE_KEY_MUMBAI],
+  };
+}
+
+if (POLYGON_RPC_URL_MATIC && PRIVATE_KEY_MATIC) {
+  (config.networks as any).matic = {
+    url: POLYGON_RPC_URL_MATIC,
+    accounts: [PRIVATE_KEY_MATIC],
+  };
+}
+
 if (ETHERSCAN_API_KEY) {
   (config.etherscan as any).apiKey = ETHERSCAN_API_KEY;
 }
@@ -51,11 +69,16 @@ export default config as HardhatUserConfig;
 
 const MAINNET_TOKEN_METADATA_URI = "https://cryptocorgis.co/api/token-metadata/{id}";
 const RINKEBY_TOKEN_METADATA_URI = "https://rinkeby.cryptocorgis.co/api/token-metadata/{id}";
+const MUMBAI_TOKEN_METADATA_URI = "https://mumbai.cryptocorgis.co/api/token-metadata/{id}";
+const MATIC_TOKEN_METADATA_URI = "https://matic.cryptocorgis.co/api/token-metadata/{id}";
 const CONTRACT_METADATA_URI = "https://cryptocorgis.co/api/contract-metadata";
 
 task("deploy_breeder", "Deploy the CryptoCorgisBreeder smart contract", async (args, hre) => {
   const breederFactory = await hre.ethers.getContractFactory("CryptoCorgisBreeder");
-  const tokenMetadataUri = hre.network.name === "rinkeby" ? RINKEBY_TOKEN_METADATA_URI : MAINNET_TOKEN_METADATA_URI;
+  const tokenMetadataUri = hre.network.name === "rinkeby" ? RINKEBY_TOKEN_METADATA_URI 
+  : hre.network.name === "mumbai" ? MUMBAI_TOKEN_METADATA_URI
+  : hre.network.name === "matic" ? MATIC_TOKEN_METADATA_URI
+  : MAINNET_TOKEN_METADATA_URI ;
   const breeder = await breederFactory.deploy(tokenMetadataUri, CONTRACT_METADATA_URI, CORGI_TREASURY_ADDRESS);
   await breeder.deployed();
   console.log(
